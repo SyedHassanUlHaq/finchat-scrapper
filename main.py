faa400712658f685a683890d37bd66f566355682
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-04-19 23:41:36 +0500

    moved heading to index 1 instead of transcript

--- main.py
+++ main.py
@@ -141,22 +141,26 @@ async def scrape_event_names(ticker, url, test_run):
                     print(f"total tabs in event {i}:", total_tabs)
                     for index in range(1, total_tabs + 1):
                         content_name = None
-                        # heading = await get_transcript_text(page)
+                        if index == 1:
+                            heading = await get_transcript_text(page)
+                            periodicity = get_periodic_from_text(heading)
+                            published_date = extract_date_from_text(heading)
+                            
                         # print('HEADING: ', heading, '\n\n\n')
                         content_type = await switch_tab(page, index=index, event=i) 
                         print(f"  [Content Type] {content_type}")                   
 
                         if content_type == "transcript":
-                            heading = await get_transcript_text(page)
+                            # heading = await get_transcript_text(page)
                             if not heading:
                                 print("  [Skipped] No heading found.")
                                 continue
 
                             content_name = heading
 
+                            # periodicity = get_periodic_from_text(heading)
+                            # published_date = extract_date_from_text(heading)
 
-                            periodicity = get_periodic_from_text(heading)
-                            published_date = extract_date_from_text(heading)
                             print(f"  [Published Date] {published_date}")
 
                             transcript_name = await download_transcript(page, event=i)
