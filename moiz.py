9653fb9a9878aba545ba6cab378f791fd9e85025
Abdul Moiz <abdulmoiznoman08@gmail.com>
2025-04-21 05:34:38 +0500

    move equity to completed folder 6.0

--- moiz.py
+++ moiz.py
@@ -58,39 +58,41 @@ lis = [
 
 
         #48-53
-        ("NCLH", "https://finchat.io/company/NYSE-NCLH/investor-relations/", "false"),
-        ("HAS", "https://finchat.io/company/NasdaqGS-HAS/investor-relations/", "false"),
-        ("WYNN", "https://finchat.io/company/NasdaqGS-WYNN/investor-relations/", "false"),
-        ("MGM", "https://finchat.io/company/NYSE-MGM/investor-relations/", "false"),
-        ("MHK", "https://finchat.io/company/NYSE-MHK/investor-relations/", "false")
-        ("CZR", "https://finchat.io/company/NasdaqGS-CZR/investor-relations/", "false")
+        # ("NCLH", "https://finchat.io/company/NYSE-NCLH/investor-relations/", "false"),
+        # ("HAS", "https://finchat.io/company/NasdaqGS-HAS/investor-relations/", "false"),
+        # ("WYNN", "https://finchat.io/company/NasdaqGS-WYNN/investor-relations/", "false"),
+        # ("MGM", "https://finchat.io/company/NYSE-MGM/investor-relations/", "false"),
+        # ("MHK", "https://finchat.io/company/NYSE-MHK/investor-relations/", "false"),
+        # ("CZR", "https://finchat.io/company/NasdaqGS-CZR/investor-relations/", "false"),
 
 
         #160-183
-        ("VRSN", "https://finchat.io/company/NasdaqGS-VRSN/investor-relations/", "false"),
-        ("MCHP", "https://finchat.io/company/NasdaqGS-MCHP/investor-relations/", "false"),
-        ("ZM", "https://finchat.io/company/NasdaqGS-ZM/investor-relations/", "false"),
-        ("ZS", "https://finchat.io/company/NasdaqGS-ZS/investor-relations/", "false"),
-        ("HPE", "https://finchat.io/company/NYSE-HPE/investor-relations/", "false"),
-        ("PTC", "https://finchat.io/company/NasdaqGS-PTC/investor-relations/", "false"),
-        ("SMCI", "https://finchat.io/company/NasdaqGS-SMCI/investor-relations/", "false"),
-        ("NTAP", "https://finchat.io/company/NasdaqGS-NTAP/investor-relations/", "false"),
-        ("OKTA", "https://finchat.io/company/NasdaqGS-OKTA/investor-relations/", "false"),
-        ("PINS", "https://finchat.io/company/NYSE-PINS/investor-relations/", "false"),
-        ("NTNX", "https://finchat.io/company/NasdaqGS-NTNX/investor-relations/", "false"),
-        ("GWRE", "https://finchat.io/company/NYSE-GWRE/investor-relations/", "false"),
-        ("DOCU", "https://finchat.io/company/NasdaqGS-DOCU/investor-relations/", "false"),
-        ("FFIV", "https://finchat.io/company/NasdaqGS-FFIV/investor-relations/", "false"),
-        ("TOST", "https://finchat.io/company/NYSE-TOST/investor-relations/", "false"),
-        ("ON", "https://finchat.io/company/NasdaqGS-ON/investor-relations/", "false"),
-        ("JBL", "https://finchat.io/company/NYSE-JBL/investor-relations/", "false"),
-        ("GEN", "https://finchat.io/company/NasdaqGS-GEN/investor-relations/", "false"),
-        ("TWLO", "https://finchat.io/company/NYSE-TWLO/investor-relations/", "false"),
-        ("DT", "https://finchat.io/company/NYSE-DT/investor-relations/", "false"),
-        ("PSTG", "https://finchat.io/company/NYSE-PSTG/investor-relations/", "false"),
-        ("WDC", "https://finchat.io/company/NasdaqGS-WDC/investor-relations/", "false"),
-        ("TER", "https://finchat.io/company/NasdaqGS-TER/investor-relations/", "false"),
-        ("MDB", "https://finchat.io/company/NasdaqGM-MDB/investor-relations/", "false"),
+        # ("VRSN", "https://finchat.io/company/NasdaqGS-VRSN/investor-relations/", "false"),
+        # ("MCHP", "https://finchat.io/company/NasdaqGS-MCHP/investor-relations/", "false"),
+        # ("ZM", "https://finchat.io/company/NasdaqGS-ZM/investor-relations/", "false"),
+        # ("ZS", "https://finchat.io/company/NasdaqGS-ZS/investor-relations/", "false"),
+        # ("HPE", "https://finchat.io/company/NYSE-HPE/investor-relations/", "false"),
+        # ("PTC", "https://finchat.io/company/NasdaqGS-PTC/investor-relations/", "false"),
+        # ("SMCI", "https://finchat.io/company/NasdaqGS-SMCI/investor-relations/", "false"),
+        # ("NTAP", "https://finchat.io/company/NasdaqGS-NTAP/investor-relations/", "false"),
+        # ("OKTA", "https://finchat.io/company/NasdaqGS-OKTA/investor-relations/", "false"),
+        # ("PINS", "https://finchat.io/company/NYSE-PINS/investor-relations/", "false"),
+        # ("NTNX", "https://finchat.io/company/NasdaqGS-NTNX/investor-relations/", "false"),
+        # ("GWRE", "https://finchat.io/company/NYSE-GWRE/investor-relations/", "false"),
+        # ("DOCU", "https://finchat.io/company/NasdaqGS-DOCU/investor-relations/", "false"),
+        # ("FFIV", "https://finchat.io/company/NasdaqGS-FFIV/investor-relations/", "false"),
+        # ("TOST", "https://finchat.io/company/NYSE-TOST/investor-relations/", "false"),
+        # ("ON", "https://finchat.io/company/NasdaqGS-ON/investor-relations/", "false"),
+        # ("JBL", "https://finchat.io/company/NYSE-JBL/investor-relations/", "false"),
+        # ("GEN", "https://finchat.io/company/NasdaqGS-GEN/investor-relations/", "false"),
+        # ("TWLO", "https://finchat.io/company/NYSE-TWLO/investor-relations/", "false"),
+
+
+        # ("DT", "https://finchat.io/company/NYSE-DT/investor-relations/", "false"),
+        # ("PSTG", "https://finchat.io/company/NYSE-PSTG/investor-relations/", "false"),
+        # ("WDC", "https://finchat.io/company/NasdaqGS-WDC/investor-relations/", "false"),
+        # ("TER", "https://finchat.io/company/NasdaqGS-TER/investor-relations/", "false"),
+        # ("MDB", "https://finchat.io/company/NasdaqGM-MDB/investor-relations/", "false"),
        ]
 
 for i in lis:
