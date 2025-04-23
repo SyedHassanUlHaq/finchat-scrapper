a761cd2ed7e6eaf15add764652c8a28318dd3e28
server <abdulrauf_99@outlook.com>
2025-04-23 22:25:41 +0500

    JSONS

--- ahsan.py
+++ ahsan.py
@@ -1,8 +1,8 @@
 from main import scrape_event_names
 import asyncio
 # 3-22 % 43-47 
-lis = [("NXPI", "https://finchat.io/company/NasdaqGS-NXPI/investor-relations/", "false"),
- ]
+lis = [("WBA", "https://finchat.io/company/NasdaqGS-WBA/investor-relations/", "false")
+]
 
 for i in lis:
-    asyncio.run(scrape_event_names(i[0], i[1], i[2]))
+    asyncio.run(scrape_event_names(i[0], i[1], i[2])) 
